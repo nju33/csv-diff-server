59f931dae4d345facfd61543acded9ee03fe4f6c
nju33 <nju33.ki@gmail.com>
2018-09-07 15:18:44 +0900

    fix

--- server/api.router.js
+++ server/api.router.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const verifyParams = require('./verify-params');
 const getContent = require('./get-content');
+const timeout = require('connect-timeout');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
 router.use(verifyParams(['pat', 'filename', 'ref']));
+router.use(timeout(1000 * 60 * 60));
 
 router.get('/table', async (req, res) => {
   const content = await getContent(req.query);
